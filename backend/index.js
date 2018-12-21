b840446e39c66d75ddce665ae950a24b4fc56c6c
hideki <viethungpro97@gmail.com>
2018-12-21 12:18:53 +0700

    complete

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,16 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT||3000
 const mongoose = require('mongoose')
+const MONGO_DB_USER = 'admin'
+const MONGO_DB_PASSWORD = 'admin111111'
+
 
 mongoose.set('useCreateIndex', true)
-mongoose.connect("mongodb://localhost/real-world", {useNewUrlParser: true})
+// mongoose.connect("mongodb://localhost/real-world", {useNewUrlParser: true})
+mongoose.connect('mongodb://ds141294.mlab.com:41294/real-world' ,{auth: {
+    user: MONGO_DB_USER,
+    password: MONGO_DB_PASSWORD
+  },useNewUrlParser: true})
 require('./models/User')
 require('./models/Article')
 require('./models/Comment')
